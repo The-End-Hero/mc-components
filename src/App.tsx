e8a0dc58a0f81bf31a7c0f0db2aeb273c886d3be
The-End-Hero <527409987@qq.com>
2018-09-10 23:17:03 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,166 @@
 import * as React from 'react';
 import './App.css';
 import SnackBarDemo from './routeCom/SnackBarDemo'
-import { Route} from 'react-router-dom'
-import {withStyles} from '@material-ui/core/styles';
+import {Route} from 'react-router-dom'
+import {withStyles , Theme, StyleRulesCallback } from '@material-ui/core/styles';
+import classNames from 'classnames';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
-const styles = {
+const drawerWidth = 240;
+
+const styles:StyleRulesCallback = (theme: Theme) => ({
     root: {
         flexGrow: 1,
+        height: 440,
+        zIndex: 1,
+        overflow: 'hidden',
+        position: 'relative',
+        display: 'flex',
     },
-};
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginLeft: 12,
+        marginRight: 36,
+    },
+    hide: {
+        display: 'none',
+    },
+    drawerPaper: {
+        position: 'relative',
+        whiteSpace: 'nowrap',
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerPaperClose: {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing.unit * 7,
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing.unit * 9,
+        },
+    },
+    toolbar: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+    },
+    content: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing.unit * 3,
+    },
+});
 
-class App extends React.Component {
+class App extends React.Component<any,{}> {
     public state = {
         value: 0,
+        open: false,
     }
     public handleChange = (event: any, value: any) => {
         this.setState({value});
     };
+    public handleDrawerOpen = () => {
+        this.setState({ open: true });
+    };
 
+    public handleDrawerClose = () => {
+        this.setState({ open: false });
+    };
     public render() {
+        const {classes, theme} = this.props;
         return (
             <div className="App">
-                <Paper>
-                    <Tabs
-                        value={this.state.value}
-                        onChange={this.handleChange}
-                        centered={true}
-                    >
-                        <Tab label="SnackBarDemo" href="#/SnackBarDemo"/>
-                        <Tab label="Item Two"/>
-                        <Tab label="Item Three"/>
-                    </Tabs>
-                </Paper>
-
                 <div>
                     <Route exact={true} path="/SnackBarDemo" component={SnackBarDemo}/>
                 </div>
+
+                <AppBar
+                    position="absolute"
+                    className={classNames(classes.appBar, this.state.open && classes.appBarShift)}
+                >
+                    <Toolbar disableGutters={!this.state.open}>
+                        <IconButton
+                            color="inherit"
+                            aria-label="Open drawer"
+                            onClick={this.handleDrawerOpen}
+                            className={classNames(classes.menuButton, this.state.open && classes.hide)}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="title" color="inherit" noWrap={true}>
+                            Mini variant drawer
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                    variant="permanent"
+                    classes={{
+                        paper: classNames(classes.drawerPaper, !this.state.open && classes.drawerPaperClose),
+                    }}
+                    open={this.state.open}
+                >
+                    <div className={classes.toolbar}>
+                        <IconButton onClick={this.handleDrawerClose}>
+                            {theme.direction === 'rtl' ? <ChevronRightIcon/> : <ChevronLeftIcon/>}
+                        </IconButton>
+                    </div>
+                    <Divider/>
+                    <List>sdsdsd</List>
+                    <Divider/>
+                    <List>eg4g4g4g4r4rt4</List>
+                    <Paper>
+                        <Tabs
+                            value={this.state.value}
+                            onChange={this.handleChange}
+                            centered={true}
+                        >
+                            <Tab label="SnackBarDemo" href="#/SnackBarDemo"/>
+                            <Tab label="Item Two"/>
+                            <Tab label="Item Three"/>
+                        </Tabs>
+                    </Paper>
+                </Drawer>
+                <main className={classes.content}>
+                    <div className={classes.toolbar}/>
+                    <Typography noWrap={true}>{'You think water moves fast? You should see ice.'}</Typography>
+                </main>
             </div>
         );
     }
 }
 
-export default withStyles(styles)(App);
+export default withStyles(styles, {withTheme: true})(App);
